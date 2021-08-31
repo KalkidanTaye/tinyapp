7c7330dfeeb0c03b874ff29ca9aef14f22dd758a
KalkidanTaye <kali.taye@gmail.com>
2021-08-31 08:14:28 +0000

    Add a header Partial

--- express_server.js
+++ express_server.js
@@ -29,6 +29,9 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-    const templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here? */ };
-    res.render("urls_show", templateVars);
-  });
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: req.params.longURL,
+  };
+  res.render("urls_show", templateVars);
+});
