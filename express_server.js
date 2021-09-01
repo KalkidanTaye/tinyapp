ac07dc20e6c52058cb8834e420d873e8949cebb6
KalkidanTaye <kali.taye@gmail.com>
2021-09-01 22:25:10 +0000

    add login and logout client and server logic

--- express_server.js
+++ express_server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require("cookie-parser");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 function generateRandomString() {
@@ -38,7 +39,8 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+
   res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => {
@@ -48,6 +50,7 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
+    username: req.cookies["username"],
     longURL: req.params.longURL,
   };
   res.render("urls_show", templateVars);
@@ -75,6 +78,7 @@ app.post("/urls/:shortURL/update", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: currentLongURL,
+    username: req.cookies["username"],
   };
   res.render("urls_show", templateVars);
 });
@@ -100,5 +104,12 @@ app.post("/urls/:shortURL", (req, res) => {
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+// logout
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
