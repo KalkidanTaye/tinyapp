3ab2621c502bfb3549aa0cf928e98aa1f396158c
KalkidanTaye <kali.taye@gmail.com>
2021-09-01 20:11:24 +0000

    Add login functionality to the app

--- express_server.js
+++ express_server.js
@@ -94,3 +94,11 @@ app.post("/urls/:shortURL", (req, res) => {
   // redirect to /quotes
   res.redirect("/urls");
 });
+
+//login a random User
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
